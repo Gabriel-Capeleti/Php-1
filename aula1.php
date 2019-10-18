5d97a0bf2c0a40f47f8b7f7b224b98fed60f1179
SaturnDraco61 <56743834+SaturnDraco61@users.noreply.github.com>
2019-10-18 20:13:03 -0300

    a
    a

---
+++ aula1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Soma com valores Definidos</title>
+</head>
+<body>
+    
+    <h1>Efetuando uma soma com PHP</h1>
+     
+    <?php
+
+        // Variáveis
+        $num1=10;
+        $num2=10;
+
+        // Efetua a soma
+        $total = $num1+$num2;
+
+        // Mostrando Resultado
+        echo "O Resultado da soma é $total";
+    
+    ?>    
+
+</body>
+</html>
